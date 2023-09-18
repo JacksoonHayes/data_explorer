dfaece7da0b8ab2bac339a20d4b0e60582c01063
JacksoonHayes <jackson-hayes@live.nmit.ac.nz>
2023-09-18 12:36:47 +1200

    login page finished, or mostly complete

--- login.py
+++ login.py
@@ -8,14 +8,15 @@ sg.theme('DarkBlue4')
 
 # Define the window's contents
 layout = [
-    [sg.Text("Login"), sg.Text(size=(15,1))],
-    [sg.Text("Username:"), sg.Input(key='-USERNAME-')],
-    [sg.Text("Password:"),sg.Input(key='-PASSWORD-', password_char='*')],
-    [sg.Text(size=(40,1), key='-OUTPUT-')],
-    [sg.Button('Submit'), sg.Button('Quit')]
+    [sg.T("Login", font="any 16")],
+    [sg.HSeparator(pad=(70, 20))],
+    [sg.Text("Username:"), sg.Input(key='-USERNAME-', size=(18,1))],
+    [sg.Text("Password:"),sg.Input(key='-PASSWORD-', password_char='•', size=(18,1))],
+    [sg.Text(size=(40,1), key='-OUTPUT-', pad=(0, 30), justification='c')],
+    [sg.Button('Submit',), sg.Button('Quit')]
 ]
 
-window = sg.Window('Login', layout, size=(400, 300))
+window = sg.Window('Login screen', layout, size=(400, 300), element_justification='c', margins=(0, 20))
 
 # Display and interact with the Window using an Event Loop
 while True:
@@ -24,7 +25,7 @@ while True:
     if event == sg.WINDOW_CLOSED or event == 'Quit':
         break
     # Output a message to the window
-    window['-OUTPUT-'].update('Hello ' + values['-USERNAME-'] + "! Thanks for trying PySimpleGUI")
+    window['-OUTPUT-'].update('Hello ' + values['-USERNAME-'] + ", Login successful!")
 
 # Finish up by removing from the screen
 window.close()
