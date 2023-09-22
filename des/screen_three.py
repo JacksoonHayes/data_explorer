b6d7b080ae7fd693d169d6ad58ff3e9da6178a5c
JacksoonHayes <jackson-hayes@live.nmit.ac.nz>
2023-09-22 22:13:53 +1200

    zoom changed

--- des/screen_three.py
+++ des/screen_three.py
@@ -74,9 +74,14 @@ def make_screen_three():
             break
 
         if event == 'Zoom In':
-            ax.set_xlim(0, 10)
-            ax.set_ylim(0, 10)
-            canvas.draw()
+            if ax.get_xlim()[1] == 10 and ax.get_ylim()[1] == 10:
+                ax.set_xlim(0, 5)
+                ax.set_ylim(0, 5)
+                canvas.draw()
+            else:
+                ax.set_xlim(0, 10)
+                ax.set_ylim(0, 10)
+                canvas.draw()
 
         if event == 'Zoom Out':
             ax.set_xlim(-5, 15)
